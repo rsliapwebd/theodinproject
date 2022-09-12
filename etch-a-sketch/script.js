53f9f189934bf2c219be7a9f11f4c4c7c71ed70e
rsliapwebd <rsliapwebd@gmail.com>
2022-09-12 18:00:17 +0300

    Add coloring

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -3,14 +3,61 @@
 const gridContainer = document.querySelector(".grid-container");
 
 const gridCell = document.createElement("div");
-gridCell.style.width = `${60 / 4}rem`;
-gridCell.style.height = `${60 / 4}rem`;
 gridCell.classList.add("grid-cell");
 
+const customGridBtn = document.querySelector(".btn-custom");
+const resetGridBtn = document.querySelector(".btn-reset");
+
+// DEFAULT GRID LOAD
 const defaultGridLoad = function () {
-  for (let i = 0; i < 16; i++) {
+  gridCell.style.width = `${60 / 16}rem`;
+  gridCell.style.height = `${60 / 16}rem`;
+
+  for (let i = 0; i < 256; i++) {
     gridContainer.appendChild(gridCell.cloneNode(true));
   }
 };
 
+// CLEAR GRiD
+const clearGrid = function () {
+  gridContainer.innerHTML = "";
+};
+
+// RESET GRID
+const resetGrid = function () {
+  clearGrid();
+  defaultGridLoad();
+  cellColoring();
+};
+
+// CUSTOM GRID LOAD
+const customGridLoad = function () {
+  clearGrid();
+  const chooseDimensions = prompt("Choose dimensions of a grid: 0-100");
+
+  gridCell.style.width = `${60 / chooseDimensions}rem`;
+  gridCell.style.height = `${60 / chooseDimensions}rem`;
+
+  for (let i = 0; i < chooseDimensions ** 2; i++) {
+    gridContainer.appendChild(gridCell.cloneNode(true));
+  }
+
+  cellColoring();
+};
+
 defaultGridLoad();
+
+resetGridBtn.addEventListener("click", resetGrid);
+customGridBtn.addEventListener("click", customGridLoad);
+
+const cellColoring = function () {
+  const allGridCells = document.querySelectorAll(".grid-cell");
+
+  allGridCells.forEach((gridCell) =>
+    gridCell.addEventListener("mouseenter", function () {
+      gridCell.classList.add("colored");
+    })
+  );
+};
+
+cellColoring();
