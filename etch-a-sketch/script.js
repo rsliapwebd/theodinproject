ef27cd5f93112253626f6e229049c175573c6a6b
rsliapwebd <rsliapwebd@gmail.com>
2022-09-12 17:07:43 +0300

    Add default grid functionality

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const gridContainer = document.querySelector(".grid-container");
+
+const gridCell = document.createElement("div");
+gridCell.style.width = `${60 / 4}rem`;
+gridCell.style.height = `${60 / 4}rem`;
+gridCell.classList.add("grid-cell");
+
+const defaultGridLoad = function () {
+  for (let i = 0; i < 16; i++) {
+    gridContainer.appendChild(gridCell.cloneNode(true));
+  }
+};
+
+defaultGridLoad();
