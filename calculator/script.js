ea546ec8cbf0cdbf7936ff5521ef72af803496b1
rsliapwebd <rsliapwebd@gmail.com>
2022-09-13 15:13:00 +0300

    Add minor changes

--- calculator/script.js
+++ calculator/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const display = document.querySelector(".display");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    display.textContent += btn.textContent;
+  })
+);
