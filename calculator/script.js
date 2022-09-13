7b29ac6a812908a1575821cd0a870c924134f81d
rsliapwebd <rsliapwebd@gmail.com>
2022-09-13 15:40:17 +0300

    Add operators

--- calculator/script.js
+++ calculator/script.js
@@ -1,10 +1,16 @@
 "use strict";
 
 const display = document.querySelector(".display");
-const btns = document.querySelectorAll(".btn");
+const nums = document.querySelectorAll(".num");
 
-btns.forEach((btn) =>
-  btn.addEventListener("click", function () {
-    display.textContent += btn.textContent;
+const numberArray = [];
+let stringNumber = "";
+
+nums.forEach((num) =>
+  num.addEventListener("click", function () {
+    display.textContent += num.textContent;
+    stringNumber += num.textContent;
   })
 );
+
+const operators = document.querySelectorAll(".operator");
