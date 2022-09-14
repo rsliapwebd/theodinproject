8beefb7b6eaf0a5c8db7cabdf337e3930fccca70
rsliapwebd <rsliapwebd@gmail.com>
2022-09-14 13:15:33 +0300

    Add math functions

--- calculator/script.js
+++ calculator/script.js
@@ -6,6 +6,20 @@ const nums = document.querySelectorAll(".num");
 const numberArray = [];
 let stringNumber = "";
 
+// Math functions
+const multiply = function (num1, num2) {
+  return num1 * num2;
+};
+const divide = function (num1, num2) {
+  return num1 / num2;
+};
+const add = function (num1, num2) {
+  return Number(num1) + Number(num2);
+};
+const substract = function (num1, num2) {
+  return num1 - num2;
+};
+
 nums.forEach((num) =>
   num.addEventListener("click", function () {
     display.textContent += num.textContent;
@@ -14,3 +28,29 @@ nums.forEach((num) =>
 );
 
 const operators = document.querySelectorAll(".operator");
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", function () {
+    display.textContent += operator.textContent;
+    numberArray.push(stringNumber);
+    stringNumber = "";
+  });
+});
+
+const equal = document.querySelector(".equal");
+
+equal.addEventListener("click", function () {
+  let displayStr = display.textContent;
+  numberArray.push(stringNumber);
+  stringNumber = "";
+
+  if (displayStr.includes("x")) {
+    display.textContent = multiply(...numberArray);
+  } else if (displayStr.includes("/")) {
+    display.textContent = divide(...numberArray);
+  } else if (displayStr.includes("+")) {
+    display.textContent = add(...numberArray);
+  } else if (displayStr.includes("-")) {
+    displayStr.textContent = substract(...numberArray);
+  }
+});
