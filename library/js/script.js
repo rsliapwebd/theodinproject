19a74079da9bf2ae007cc36d9ca292b42cdf0a78
rsliapwebd <rsliapwebd@gmail.com>
2022-10-05 15:36:26 +0300

    TODO: read button toggle

--- library/js/script.js
+++ library/js/script.js
@@ -17,21 +17,49 @@ const Book = function (title, author, numPages) {
   this.title = title;
   this.author = author;
   this.numPages = numPages;
-  this.readStatus = "not read";
+  this.readStatus = "Not read";
 };
 
 // To create book from a form
-const createBook = function () {
-  const book = new Book(title.value, author.value, numPages.value);
+const createBook = function (inputs) {
+  const book = new Book(...inputs);
   library.push(book);
+};
+
+// Clear input fields
+const clearInputs = function () {
+  title.value = author.value = numPages.value = "";
 };
 
 addBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  if (title.value === "" || author.value === "" || numPages.value === "") {
-    alert("Fill the book form, please.");
-  } else {
-    createBook();
-    console.log(library);
-  }
+  const inputs = [title.value, author.value, numPages.value];
+  createBook(inputs);
+  console.log(library);
+  clearInputs();
+  addBooksToDisplay();
 });
+
+// Create and add books from library
+function addBooksToDisplay() {
+  libraryContainer.innerHTML = "";
+  library.forEach((book) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <h3>${book.title}</h3>
+    <h4>${book.author}</h4>
+    <h5>Pages: ${book.numPages}</h5>
+    <button class='btn book-btn'>${book.readStatus}</button>
+    `;
+    div.classList.add("book");
+    libraryContainer.appendChild(div);
+  });
+}
+
+// Read/not read toggle
+const readBtn = document.querySelectorAll(".book-btn");
+readBtn.forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    e.target.classList.toggle("read");
+  })
+);
