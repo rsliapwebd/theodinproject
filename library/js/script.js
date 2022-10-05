be6556294a680151fbd565f496418e43489c9c03
rsliapwebd <rsliapwebd@gmail.com>
2022-10-05 11:47:27 +0300

    Add adding books feature

--- library/js/script.js
+++ library/js/script.js
@@ -1,10 +1,37 @@
 "use strict";
 
+const libraryContainer = document.querySelector(".library-container");
+const bookForm = document.querySelector(".book-form");
+const title = document.querySelector(".title");
+const author = document.querySelector(".author");
+const numPages = document.querySelector(".num-pages");
+const addBtn = document.querySelector(".add-book");
+
+// FUNCTIONALITY
+
+// Array with created books
 let library = [];
 
-const Book = function (title, author, numPages, readStatus = "not read") {
+// Book constructor
+const Book = function (title, author, numPages) {
   this.title = title;
   this.author = author;
   this.numPages = numPages;
-  this.readStatus = readStatus;
+  this.readStatus = "not read";
 };
+
+// To create book from a form
+const createBook = function () {
+  const book = new Book(title.value, author.value, numPages.value);
+  library.push(book);
+};
+
+addBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (title.value === "" || author.value === "" || numPages.value === "") {
+    alert("Fill the book form, please.");
+  } else {
+    createBook();
+    console.log(library);
+  }
+});
