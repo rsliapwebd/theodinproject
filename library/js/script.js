0ff98b7f20d79832059838d7dcf7e72796386021
rsliapwebd <rsliapwebd@gmail.com>
2022-10-05 11:11:28 +0300

    Add html structure

--- library/js/script.js
+++ library/js/script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+let library = [];
+
+const Book = function (title, author, numPages, readStatus = "not read") {
+  this.title = title;
+  this.author = author;
+  this.numPages = numPages;
+  this.readStatus = readStatus;
+};
