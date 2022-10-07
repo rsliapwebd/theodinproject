f4c9f594203b0950f00da6623bb2b4c7f6d13bdf
rsliapwebd <rsliapwebd@gmail.com>
2022-10-07 10:45:51 +0300

    Add grid rendering

--- tic-tac-toe/js/script.js
+++ tic-tac-toe/js/script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const cells = document.querySelectorAll(".cell");
+
+const gameBoard = (function () {
+  let marks = ["", "", "", "", "", "", "", "", ""];
+  return { marks };
+})();
+
+const players = (function () {
+  let activePlayer = "X";
+  return { activePlayer };
+})();
+
+const playing = (function () {
+  markCell();
+})();
+
+// Mark cell and add marker in marks
+function markCell() {
+  cells.forEach((cell, index) =>
+    cell.addEventListener("click", function (e) {
+      e.target.textContent = players.activePlayer;
+      gameBoard.marks[index] = players.activePlayer;
+      players.activePlayer = switchPlayer();
+    })
+  );
+}
+
+// Switch Player
+function switchPlayer() {
+  console.log(players.activePlayer);
+  return players.activePlayer === "X" ? "O" : "X";
+}
